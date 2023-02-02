1374a95995c35930224672ea10e5357ec3d61e41
Lorenz Schmid <lorenz-schmid@gmx.net>
2023-02-02 15:42:50 +0100

    user

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,1 +1,11 @@
 import mongoose from 'mongoose'; 
+
+const { Schema, model } = mongoose; 
+
+const userSchema = new Schema({
+
+});
+
+const User = model('User', userSchema)
+
+export default User; 
