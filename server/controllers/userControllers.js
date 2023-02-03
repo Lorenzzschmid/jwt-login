6e43606df036c000851c20b5b5d1c9e2fe32ce12
Lorenz Schmid <lorenz-schmid@gmx.net>
2023-02-03 15:19:37 +0100

    register

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt'; 
+import User from '../models/userModel.js'
 
 
 export const findAllUsers = async (req, res) => {
@@ -6,3 +7,38 @@ export const findAllUsers = async (req, res) => {
 
     return res.status(200).json({ users })
 };
+ export const registerUser = async (req, res) => {
+    try {
+        const { firstName, lastName, userName, email, password } = req.body; 
+        const hashedPassword = await bcrypt.hash(password, 10); 
+        const userUsernameExists = await User.findOne({userName}); 
+        const userEmailExists = await User.findOne({email}); 
+
+        if(userEmailExists) {
+            return res.status(400).json({ message: "This Email is already taken"}); 
+        } else if (userUsernameExists) {
+            return res.status(400).json({ message: "Username already taken"}); 
+        }
+        
+        const createdUser = await User.create({
+            firstName,
+            lastName,
+            email,
+            userName,
+            password: hashedPassword
+        }); 
+        console.log('Success! User registered 👌️'); 
+
+        return res
+        .status(200)
+        .json({ message: 'Created successfully', createdUser });
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+ };
+
+ export const loginUser = async (req, res) => {
+    const { email, password} = req.body; 
+
+    
+ }
