de757d55af7b5f0c0df214167748a656d1d88045
Lorenz Schmid <lorenz-schmid@gmx.net>
2023-02-04 14:32:22 +0100

    delete User

--- server/controllers/userControllers.js
+++ server/controllers/userControllers.js
@@ -103,4 +103,17 @@ export const findAllUsers = async (req, res) => {
     }
  }; 
 
- 
+ export const deleteUser = async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id); 
+        if (!user) {
+            return res.status(400).json({message: 'User not found'}); 
+        }
+
+        return res
+        .status(200)
+        .json({message: 'User successfully deleted', deletedUser: user}); 
+    } catch (error) {
+        return res.status(400).json({ message: error.message }); 
+    }
+ }; 
